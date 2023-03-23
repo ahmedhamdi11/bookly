86650c7d7aae22e8ecc0b8982e2b97f694e11381
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-03-23 20:00:44 +0200

    create styles file and adding googl fonts package

--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utile/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
@@ -9,9 +10,20 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: const [
         CustomAppBar(),
         FeaturedBooks(),
+        SizedBox(
+          height: 50,
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 24.0),
+          child: Text(
+            'Best Seller',
+            style: Styles.titleMedume,
+          ),
+        ),
       ],
     );
   }
---
+++ lib/core/utile/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const titleMedume = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+}
