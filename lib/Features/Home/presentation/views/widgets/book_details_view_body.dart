70d701eb64f8585744bd46d826ceb5d2ee239443
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-03-25 20:46:40 +0200

    create book details image

--- lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -1,12 +1,21 @@
+import 'package:bookly/Features/Home/presentation/views/widgets/book_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.45,
+            child: const BookImage(),
+          )
+        ],
+      ),
+    );
   }
 }
