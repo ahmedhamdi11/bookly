91531f35b9e264ce18d6c2e4110c4fa45ec3a240
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-03-26 21:17:31 +0200

    dio package added & api services class created

---
+++ lib/core/utile/api_service.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+class ApiServices {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final Dio dio;
+  ApiServices(this.dio);
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    Response response = await dio.get('$_baseUrl$endPoint');
+    return response.data;
+  }
+}
