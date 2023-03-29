1fed477d11a0e76b27e30b7710cb5a0815148dab
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-03-29 20:32:04 +0200

    setUp GetIt done

---
+++ lib/core/utile/service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/Features/Home/data/repos/home_repo_impl.dart';
+import 'package:bookly/core/utile/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setUpGetIt() {
+  getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(getIt.get<ApiServices>()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utile/app_router.dart';
+import 'package:bookly/core/utile/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setUpGetIt();
   runApp(const BooklyApp());
 }
 
