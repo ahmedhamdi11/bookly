d097eacbb74bc8c877eedfc554890a102008a562
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-03-30 01:55:32 +0200

    adding cached network image

--- lib/Features/Home/presentation/views/widgets/book_image.dart
+++ lib/Features/Home/presentation/views/widgets/book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class BookImage extends StatelessWidget {
@@ -12,15 +13,16 @@ class BookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.05 / 3,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(borderRadius),
-          image: DecorationImage(
-              image: NetworkImage(
-                imageUrl ??
-                    'https://cor-cdn-static.bibliocommons.com/assets/default_covers/icon-book-93409e4decdf10c55296c91a97ac2653.png',
-              ),
-              fit: BoxFit.fill),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(borderRadius),
+        child: CachedNetworkImage(
+          imageUrl: imageUrl ??
+              'https://cor-cdn-static.bibliocommons.com/assets/default_covers/icon-book-93409e4decdf10c55296c91a97ac2653.png',
+          fit: BoxFit.fill,
+          placeholder: (context, url) => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
       ),
     );
