2e181b953b32c97db35e7753ef172488b400ea84
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-03-30 07:00:44 +0200

    similar books cubit done

---
+++ lib/Features/Home/presentation/manager/cubit/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/Home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+  final HomeRepo homeRepo;
+  Future fetchSimilarBooks() async {
+    emit(SimilarBooksLoadingState());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((failure) {
+      emit(SimilarBooksFailureState(failure.errorMessage));
+    }, (books) {
+      emit(SimilarBooksSuccessState(books));
+    });
+  }
+}
