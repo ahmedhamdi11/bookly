d65975da4f66f09cb8d55f9a77780b6c58725301
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-01 01:51:12 +0200

    search repo & impl done

---
+++ lib/Features/Search/data/repos/search_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:bookly/Features/Search/data/repos/search_repo.dart';
+import 'package:bookly/core/utile/api_service.dart';
+import 'package:dartz/dartz.dart';
+import 'package:bookly/core/models/book_model/book_model.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImpl implements SearchRepo {
+  ApiServices apiServices;
+  SearchRepoImpl(this.apiServices);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSearchResults(
+      {required String search}) async {
+    try {
+      var data = await apiServices.get(
+          endPoint: 'volumes?q=intitle:$search&Filtering=free-ebooks');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+}
