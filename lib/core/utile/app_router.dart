de19b9bbae3c14eae4d050b68ce65321cce03182
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-02 22:47:43 +0200

    splashView feature deleted

--- lib/core/utile/app_router.dart
+++ lib/core/utile/app_router.dart
@@ -6,21 +6,15 @@ import 'package:bookly/Features/Home/data/repos/home_repo_impl.dart';
 import 'package:bookly/Features/Home/presentation/manager/cubit/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly/Features/Home/presentation/views/book_details_view.dart';
 import 'package:bookly/Features/Home/presentation/views/home_view.dart';
-import 'package:bookly/Features/Splash/presentation/views/splash_view.dart';
 import 'package:bookly/core/utile/service_locator.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
-  static String homeViewPath = '/homeView';
   static String bookDetailsViewPath = '/bookDetailsView';
   static String searchViewPath = '/searchView';
   static final router = GoRouter(
     routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const SplashView(),
-      ),
       GoRoute(
         path: searchViewPath,
         builder: (context, state) => BlocProvider(
@@ -30,7 +24,7 @@ abstract class AppRouter {
         ),
       ),
       GoRoute(
-        path: homeViewPath,
+        path: '/',
         builder: (context, state) => const HomeView(),
       ),
       GoRoute(
