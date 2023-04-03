69d60c4b62d32ce7536e02037ca0917c258d36fd
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-04-03 05:33:00 +0200

    adding textStyle to the errMessage

--- lib/Features/Home/presentation/views/widgets/failure_ui.dart
+++ lib/Features/Home/presentation/views/widgets/failure_ui.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/Home/presentation/manager/cubit/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/Features/Home/presentation/manager/cubit/newest_books_cubit/newest_books_cubit.dart';
+import 'package:bookly/core/utile/styles.dart';
 import 'package:bookly/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,6 +25,7 @@ class FailureUi extends StatelessWidget {
             child: Text(
               errMessage,
               textAlign: TextAlign.center,
+              style: Styles.text14,
             ),
           ),
           const SizedBox(
